892b53b726838b49ae8b580f9ed89a0a158a4f25
Drakenion <163164782+Drakenion@users.noreply.github.com>
2024-11-25 23:46:21 +0530

    Add files via upload

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Created by
+      <a
+        >Parth Jain</a
+      >, 
+    </p>
+  </div>
+  );
+} 
